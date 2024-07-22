c5d98b2172b81d00a32b6a3c85dd8e66e0346c00
shmll <shmllslmt@gmail.com>
2024-07-22 16:06:23 +0800

    Animal, Shelter

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,77 @@
-package PACKAGE_NAME;public class Animal {
+import java.util.Date;
+
+public class Animal {
+    private String id;
+    private String name;
+    private String size;
+    private int age;
+    private Date yearRescue;
+    private boolean hasSponsor;
+    private Shelter shelter;
+
+    protected Animal() {
+        this("A000", "Animal", new Shelter());
+    }
+    protected Animal(String id, String name, Shelter shelter) {
+        this.id = id;
+        this.name = name;
+        this.shelter = shelter;
+    }
+
+    public void createAnimal() {
+        shelter.addAnimal(this);
+    }
+    public void deleteAnimal() {
+        shelter.removeAnimal(this);
+    }
+    public void deleteAnimal(String id) {
+        Animal deletedAnimal = shelter.getAnimal(id);
+        if(deletedAnimal != null) {
+            shelter.removeAnimal(deletedAnimal);
+        } else {
+            System.out.println("Animal not found. Please try again.");
+        }
+    }
+    public String getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getSize() {
+        return size;
+    }
+    public int getAge() {
+        return age;
+    }
+    public Date getYearRescue() {
+        return yearRescue;
+    }
+    public boolean isHasSponsor() {
+        return hasSponsor;
+    }
+    public Shelter getShelter() {
+        return shelter;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setSize(String size) {
+        this.size = size;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setYearRescue(Date yearRescue) {
+        this.yearRescue = yearRescue;
+    }
+    public void setHasSponsor(boolean hasSponsor) {
+        this.hasSponsor = hasSponsor;
+    }
+    public void setShelter(Shelter shelter) {
+        this.shelter = shelter;
+    }
 }
--- src/Shelter.java
+++ src/Shelter.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class Shelter {
+import java.util.ArrayList;
+
+public class Shelter {
+    private String id;
+    private String name;
+    private String address;
+    private ArrayList<Animal> animals;
+
+    protected Shelter() {
+        this("S000", "Animal Shelter", "Johor");
+    }
+    protected Shelter(String id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.animals = new ArrayList<>();
+    }
+
+    public void addAnimal(Animal animal) {
+        if(animal != null) {
+            animals.add(animal);
+            System.out.println("Meet " + animal.getName() + "! Our latest addition in the animal shelter.");
+        }
+    }
+    public void removeAnimal(Animal animal) {
+        if(animals.contains(animal)) {
+            animals.remove(animal);
+            System.out.println(animal.getName() + " has been removed from the animal shelter.");
+        } else {
+            System.out.println(animal.getName() + " is not even in the animal shelter. Please try again.");
+        }
+    }
+
+    public ArrayList<Animal> getAnimals() {
+        return animals;
+    }
+    public Animal getAnimal(String id) {
+        for(Animal a: animals) {
+            if(a.getId().equals(id)) {
+                return a;
+            }
+        }
+        return null;
+    }
 }
